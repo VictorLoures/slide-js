8638f455fc0bf12f5e9663e3cda5580fba2db4d4
Victor <victor.loures@projetusti.com.br>
2023-12-16 11:57:51 -0300

    slide config

--- js/slide.js
+++ js/slide.js
@@ -56,9 +56,40 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  //slidesConfig
+
+  slidePosisition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    };
+  }
+
+  slideConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosisition(element);
+      return { element, position };
+    });
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(this.slideArray[index].position);
+    this.slidesIndexNav(index);
+    this.dist.finalPosition = activeSlide.position;
+  }
+
   init() {
     this.bindEvents();
     this.addSlideEvvents();
+    this.slideConfig();
     return this;
   }
 }
